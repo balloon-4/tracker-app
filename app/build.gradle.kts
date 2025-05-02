e418f471e7491aa2ecff761b876c063c4539ee7b
Evan F <84680760+Attituding@users.noreply.github.com>
2025-05-02 12:14:56 -0700

    ci: sign release apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 2
-        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
